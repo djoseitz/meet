66a8f5336875071b59a7f106442cb2ff0e988aaa
djoseitz <72769693+djoseitz@users.noreply.github.com>
2021-03-19 13:32:30 -0700

    updated app.js

--- src/Alert.js
+++ src/Alert.js
@@ -4,19 +4,18 @@ class Alert extends Component {
   constructor(props) {
     super(props);
     this.color = null;
-    this.className = null;
   }
+
   getStyle = () => {
     return {
       color: this.color,
     };
   };
+
   render() {
     return (
-      <div className="Alert">
-        <p style={this.getStyle()} className={this.className}>
-          {this.props.text}
-        </p>
+      <div className='Alert'>
+        <p style={this.getStyle()}>{this.props.text}</p>
       </div>
     );
   }
@@ -25,38 +24,22 @@ class Alert extends Component {
 class InfoAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = 'blue';
-    this.className = 'info';
+    this.color = '#323edd';
+  }
+}
+
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = '#FFA500';
   }
 }
 
 class ErrorAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = 'red';
-    this.className = 'error';
+    this.color = '#ec0101';
   }
-  getStyle = () => {
-    return {
-      color: this.color,
-      fontWeight: '600',
-    };
-  };
 }
 
-class WarningAlert extends Alert {
-  constructor(props) {
-    super(props);
-    this.color = 'orange';
-    this.className = 'warning';
-  }
-  getStyle = () => {
-    return {
-      color: this.color,
-      fontWeight: '600',
-      fontSize: '180px',
-    };
-  };
-}
-
-export { InfoAlert, ErrorAlert, WarningAlert };
+export { InfoAlert, WarningAlert, ErrorAlert };
--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
 import React, { Component } from 'react';
-import './App.css';
-import EventList from './EventList';
-import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import EventGenre from './EventGenre';
-import { getEvents } from './api';
-import './nprogress.css';
+import CitySearch from './CitySearch';
+import EventList from './EventList';
+import DataVisualization from './DataVisualization';
+import LoadingSpinner from './LoadingSpinner/LoadingSpinner';
 import { WarningAlert } from './Alert';
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from 'recharts';
+import { getEvents } from './api';
+import './styles/App.scss';
+import './styles/nprogress.css';
 
 class App extends Component {
   state = {
     events: [],
-    currentLocation: 'all',
     locations: [],
+    currentLocation: 'all',
+    numberOfEvents: '10',
     warningText: '',
-    numberOfEvents: 32,
+    isLoading: false,
   };
 
-  getData = () => {
-    const { locations, events } = this.state;
-    const data = locations.map((location) => {
-      const number = events.filter((event) => event.location === location)
-        .length;
-      const city = location.split(', ').shift();
-      return { city, number };
-    });
-    return data;
-  };
+  async componentDidMount() {
+    this.mounted = true;
+    if (!navigator.onLine) {
+      this.setState({
+        warningText:
+          'You are currently using the app offline and viewing data from your last visit. Data will not be up-to-date.',
+      });
+    } else {
+      this.setState({ warningText: '' });
+    }
 
+    if (this.mounted) {
+      this.updateEvents();
+    }
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  // Filters events based on location and number given in user input
   updateEvents = (location, eventCount) => {
+    this.setState({ isLoading: true }); // Start loading gif
+
     const { currentLocation, numberOfEvents } = this.state;
+    // If user selects a location from input
     if (location) {
       getEvents().then((response) => {
+        // Applies new filter for location
         const locationEvents =
           location === 'all'
             ? response.events
@@ -50,108 +57,59 @@ class App extends Component {
           events: events,
           currentLocation: location,
           locations: response.locations,
+          isLoading: false,
         });
       });
     } else {
       getEvents().then((response) => {
+        // Persists location filter from state
         const locationEvents =
           currentLocation === 'all'
             ? response.events
             : response.events.filter(
                 (event) => event.location === currentLocation
               );
-        const events = locationEvents.slice(0, eventCount);
-        return this.setState({
-          events: events,
-          numberOfEvents: eventCount,
-          locations: response.locations,
-        });
+        const numEvents = eventCount || numberOfEvents;
+        const events = locationEvents.slice(0, numEvents);
+        if (this.mounted) {
+          return this.setState({
+            events: events,
+            numberOfEvents: eventCount,
+            locations: response.locations,
+            isLoading: false,
+          });
+        }
       });
     }
   };
 
-  componentDidMount() {
-    if (!navigator.onLine) {
-      this.setState({
-        warningText:
-          'You are currently using the app offline and viewing data from your last visit. Data will not be up-to-date.',
-      });
-    } else {
-      this.setState({ warningText: '' });
-    }
-    this.mounted = true;
-    getEvents().then((response) => {
-      if (this.mounted) {
-        this.setState({
-          events: response.events,
-          locations: response.locations,
-        });
-      }
-    });
-  }
+  // Renders loading spinner while live data is being rendered
+  renderData = () => {
+    const { events, locations, isLoading } = this.state;
 
-  // async componentDidMount() {
-  //   this.mounted = true;
-  //   if (!navigator.onLine) {
-  //     this.setState({
-  //       warningText:
-  //         'You are currently using the app offline and viewing data from your last visit. Data will not be up-to-date.',
-  //     });
-  //   } else {
-  //     this.setState({ warningText: '' });
-  //   }
-
-  //   if (this.mounted) {
-  //     this.updateEvents();
-  //   }
-  // }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
+    return isLoading ? (
+      <LoadingSpinner />
+    ) : (
+      <div>
+        <DataVisualization events={events} locations={locations} />
+        <EventList events={events} />
+      </div>
+    );
+  };
 
   render() {
-    const data = this.getData().sort((a, b) => (a.city > b.city ? 1 : -1));
-    const {warningText} = this.state;
+    const { numberOfEvents, locations, warningText } = this.state;
 
     return (
-      <div className="App">
-        <CitySearch
-          updateEvents={this.updateEvents}
-          locations={this.state.locations}
-        />
+      <div className='App'>
+        <h1>MeetApp</h1>
+        <CitySearch locations={locations} updateEvents={this.updateEvents} />
         <NumberOfEvents
+          numberOfEvents={numberOfEvents}
           updateEvents={this.updateEvents}
-          numberOfEvents={this.state.numberOfEvents}
         />
         <WarningAlert text={warningText} />
-        {/* <div className="data-vis-wrapper">
-          <ResponsiveContainer height={400}>
-            <EventGenre events={this.state.events} />
-          </ResponsiveContainer>
-          <ResponsiveContainer height={400}>
-            <ScatterChart
-              margin={{
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 20,
-              }}
-            >
-              <CartesianGrid />
-              <XAxis type="category" dataKey="city" name="City" />
-              <YAxis
-                type="number"
-                dataKey="number"
-                name="Number of events"
-                allowDecimals={false}
-              />
-              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter data={data} fill="#8884d8" />
-            </ScatterChart>
-          </ResponsiveContainer>
-        </div> */}
-        <EventList events={this.state.events} />
+        {this.renderData()}
       </div>
     );
   }
